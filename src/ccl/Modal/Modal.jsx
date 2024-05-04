af4fb338440545aa977c3ca619de1da576cd051e
raunak299 <iamraunak299@gmail.com>
2024-05-05 01:21:22 +0530

    [weekday/ccl] : add modal to ccl

---
+++ src/ccl/Modal/Modal.jsx
@@ -0,0 +1,36 @@
+import { ModalContextProvider, modalContext } from "../context/ModalContext";
+import { useContext } from "react";
+import { createPortal } from "react-dom";
+import "./Modal.css";
+
+export default function Modal(props) {
+  return (
+    <ModalContextProvider>
+      <ModalTrigger>{props.modalButton()}</ModalTrigger>
+      <ModalOverlay>{props.modalContent()}</ModalOverlay>
+    </ModalContextProvider>
+  );
+}
+
+function ModalTrigger({ children }) {
+  const { toggleModal } = useContext(modalContext);
+  return <div onClick={toggleModal}>{children}</div>;
+}
+
+function ModalOverlay({ children }) {
+  const { isModalOpen, toggleModal } = useContext(modalContext);
+  return (
+    isModalOpen &&
+    createPortal(
+      <div className="modalOverlay" onClick={toggleModal}>
+        <div className="modalWrapper" onClick={(e) => e.stopPropagation()}>
+          <div className="modalHeader" onClick={toggleModal}>
+            <h3>Close</h3>
+          </div>
+          <div className="modalContent">{children}</div>
+        </div>
+      </div>,
+      document.body,
+    )
+  );
+}
