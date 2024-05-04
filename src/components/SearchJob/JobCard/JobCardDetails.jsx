55983ff3a81a4360567eaaa739fda3428b72568d
raunak299 <iamraunak299@gmail.com>
2024-05-05 02:24:03 +0530

    [weekday/refactor] : add job card details

--- src/components/SearchJob/JobCard/JobCardDetails.jsx
+++ src/components/SearchJob/JobCard/JobCardDetails.jsx
@@ -1,13 +1,15 @@
-import './JobCardDetails.css';
+import "./JobCardDetails.css";
 
-export default function JobCardDetails({jobDetailsFromCompany}){
-    return (
-        <div className="job-card-details__body">
-            <h3 className="job-card-details__about-sec-title">About Company:</h3>
-            <strong className="job-card-details__about-sec-subtitle">About us</strong>
-            {jobDetailsFromCompany && (
-              <p className="job-card-details__about-sec-details">{jobDetailsFromCompany}</p>
-            )}
-          </div>
-    )
+export default function JobCardDetails({ jobDetailsFromCompany }) {
+  return (
+    <div className="job-card-details__body">
+      <h3 className="job-card-details__about-sec-title">About Company:</h3>
+      <strong className="job-card-details__about-sec-subtitle">About us</strong>
+      {jobDetailsFromCompany && (
+        <p className="job-card-details__about-sec-details">
+          {jobDetailsFromCompany}
+        </p>
+      )}
+    </div>
+  );
 }
