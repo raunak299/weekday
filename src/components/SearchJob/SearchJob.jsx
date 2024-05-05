0af967d506a89b7159377fb0702d9eae8df2317c
raunak299 <iamraunak299@gmail.com>
2024-05-06 00:45:24 +0530

    [weekday/redux/chore] : reinstall redux

--- src/components/SearchJob/SearchJob.jsx
+++ src/components/SearchJob/SearchJob.jsx
@@ -1,10 +1,11 @@
+import { useSelector } from "react-redux";
 import { Error, Loader } from "../../ccl";
 import InfiniteScroll from "../../ccl/InfiniteScroll/InfiniteScroll";
 import useJobSearch from "../../hooks/useJobSearch/useJobSearch";
 import JobCard from "./JobCard/JobCard";
 import "./SearchJob.css";
 import SearchJobFilters from "./SearchJobFilters/SearchJobFilters";
-import { useSelector } from "react-redux";
+
 
 export default function SearchJob() {
   const { filteredJobs, loading, error, message } = useSelector(
@@ -15,7 +16,6 @@ export default function SearchJob() {
   return (
     <div className="search-job-sec">
       <SearchJobFilters />
-      {filteredJobs.length > 0 && (
         <InfiniteScroll
           fetchMore={fetchMoreJobs}
           listLength={filteredJobs.length}
@@ -32,8 +32,7 @@ export default function SearchJob() {
             </div>
           ))}
         </InfiniteScroll>
-      )}
-      {!filteredJobs.length > 0 && <h2 className="no-jobs">No Jobs Found</h2>}
+      {!filteredJobs.length > 0 && !loading && <h2 className="no-jobs">No Jobs Found</h2>}
     </div>
   );
 }
