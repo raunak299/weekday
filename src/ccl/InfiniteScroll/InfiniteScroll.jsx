22219e00ecf0c7190e5c90e55a589d5fb04191b5
raunak299 <iamraunak299@gmail.com>
2024-05-05 11:57:18 +0530

    [weekday/ccl/refactor] : fetchmore func renamed

--- src/ccl/InfiniteScroll/InfiniteScroll.jsx
+++ src/ccl/InfiniteScroll/InfiniteScroll.jsx
@@ -1,31 +1,36 @@
-import React, {useRef } from "react"
+import React, { useRef } from "react";
 
-export default function InfiniteScroll({ fetchMoreProducts, children, loadingMsg , listLength, loading,hasMore }) {
+export default function InfiniteScroll({
+  fetchMore,
+  children,
+  loadingMsg,
+  listLength,
+  loading,
+  hasMore,
+}) {
+  const observer = useRef();
+  const lastProductElement = (node) => {
+    if (loading) return;
+    if (observer.current) observer.current.disconnect();
+    observer.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasMore) {
+        fetchMore();
+      }
+    });
+    if (node) observer.current.observe(node);
+  };
 
-   const observer  = useRef();
-   const lastProductElement = (node)=>{
-        if(loading) return;
-        if(observer.current) observer.current.disconnect();
-        observer.current = new IntersectionObserver(entries=>{
-             if(entries[0].isIntersecting && hasMore){
-                fetchMoreProducts();
-             }
-        });
-        if(node) observer.current.observe(node);
-   };
-
-
-    return (
-        <div className="infinite-scroll">
-            {React.Children.map(children,(child,index)=>{
-                if(index + 1 === listLength) {
-                    return React.cloneElement(child,{
-                        ref: lastProductElement
-                    })
-                }
-                return React.cloneElement(child)
-            })}
-            {loading && hasMore && loadingMsg()}
-        </div>
-    )
+  return (
+    <div className="infinite-scroll">
+      {React.Children.map(children, (child, index) => {
+        if (index + 1 === listLength) {
+          return React.cloneElement(child, {
+            ref: lastProductElement,
+          });
+        }
+        return React.cloneElement(child);
+      })}
+      {loading && hasMore && loadingMsg()}
+    </div>
+  );
 }
