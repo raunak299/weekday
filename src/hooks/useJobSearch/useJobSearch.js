b19fbb79b6f26c2e327038a5147c259d38d52877
raunak299 <iamraunak299@gmail.com>
2024-05-06 00:16:00 +0530

    [weekday/search-job/refactor] :  useJobSearch refactored
    * add support to apply filters
    * add redux connection

--- src/hooks/useJobSearch/useJobSearch.js
+++ src/hooks/useJobSearch/useJobSearch.js
@@ -1,11 +1,15 @@
 import { useEffect, useState } from "react";
 import useGetJobsListing from "../useGetJobsListing/useGetJobsListing";
 import { LIMIT } from "../../constants";
+import { useSelector, useDispatch } from "react-redux";
+import { dataActions } from "../../store/data-slice";
 
 const useJobSearch = () => {
   const [currentPage, setCurrentPage] = useState(1);
+  const { fetchJobs, hasMore } = useGetJobsListing();
 
-  const { remoteDataState, fetchJobs, hasMore } = useGetJobsListing();
+  const dispatch = useDispatch();
+  const { filters } = useSelector((store) => store.dataStore);
 
   const fetchMoreJobs = () => {
     setCurrentPage((prevPage) => prevPage + 1);
@@ -15,8 +19,11 @@ const useJobSearch = () => {
     fetchJobs({ offset: (currentPage - 1) * LIMIT });
   }, [currentPage]);
 
+  useEffect(() => {
+    dispatch(dataActions.setFilteredJobs({ filters }));
+  }, [filters]);
+
   return {
-    remoteDataState,
     fetchMoreJobs,
     hasMore,
   };
